ff3e3202b6da4a8a79815a1a96232df2485f88ac
Zayn Zhan <zaynzhan@gmail>
2024-05-11 17:14:09 +1200

    End of section 2

---
+++ API/Entities/Product.cs
@@ -0,0 +1,32 @@
+// dimmed out because they are unnecessary, to remove them, use CTRL + .
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+            /* input "prop" then the following is generated automatically
+                accessor:
+
+                - Public: this property inside this class can be accessed from any other class in our application
+                It has to be Public because entity framework when we use that is going to come in and take a look at this class, 
+                and it needs to be able to get the set whatever this property is.
+
+                - Private: this property inside this class can be only accessed inside this particular class
+
+                - Protected: this is only accessible from this class and any class that derives from this class
+
+            */  
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description {get; set; }
+            public long Price { get; set; } // a hundred will be stored as 10000 for adding decimal in the UI
+            public string PictureUrl { get; set; }
+            public string Type { get; set; }
+            public string Brand { get; set; }
+            public int QuantityInStock { get; set; }
+
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,75 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+
+var app = builder.Build();
+
+// Middleware: the odrder is always important inside here
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // Map controllers
+            });
+
+
+
+
+// Creating Dependency Injection Scope - creates a new scope within which services can be resolved
+var scope = app.Services.CreateScope();
+// Within the scope, GetRequiredService<StoreContext>() retrieves an instance of StoreContext from the service provider. StoreContext represents a database context used to interact with the underlying database.
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+// Retrieves an instance of ILogger<Program> from the service provider. This logger can be used for logging messages from the Program class.
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+// This method ensures that the latest migrations (database schema changes) are applied to the database. If there are pending migrations, they will be executed.
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "A problem occurred during migration");
+}
+
+
+app.Run();
+
+
+
+
+
+
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
